96554e1bc3e1edce9eff70318cf2bfc4f4757a9f
mugiwara-lufi <saldon.jimzadreyll@gmail.com>
2026-05-16 21:10:36 +0800

    FINALS PIT

--- config/urls.py
+++ config/urls.py
@@ -17,6 +17,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework.authtoken import views as auth_views
+from soil_api.views import home_screen
 
 urlpatterns = [
     path('', home_screen, name='home'),
